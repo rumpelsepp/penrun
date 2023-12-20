2f87531d9d191e912393239baea2ce84997761ac
Stefan Tatschner <stefan@rumpelsepp.org>
2023-12-20 10:52:37 +0100

    Add linters

--- config.sh
+++ config.sh
@@ -6,10 +6,12 @@
 # Penrun config
 # NOTE: These variables are sourced by the shell.
 # They are not required to be exported, but could be.
+# Please note that bash arrays cannot be exported!
 
 # If this variable is set then penrun creates the artifacts folder hierarchy
 # at this location instead of $PWD.
 #
+# shellcheck disable=SC2034
 PENRUN_ARTIFACTS_BASE="$HOME/penrun-artifacts"
 
 # This variable specifies the compression tool where the output is piped
@@ -17,14 +19,17 @@ PENRUN_ARTIFACTS_BASE="$HOME/penrun-artifacts"
 # in all out-of-the-box scenarios. Argument must be a bash array.
 #
 # PENRUN_COMPRESSION_COMMAND=("gzip" "--stdout")
+
+# shellcheck disable=SC2034
 PENRUN_COMPRESSION_COMMAND=("zstd" "-T0")
 
 # Specify the extension that is added to the OUTPUT file.
+# shellcheck disable=SC2034
 PENRUN_OUTPUT_EXTENSION=".zst"
 
 # Pipe penrun output to HR
 # Argument must be a bash array.
-PENRUN_PIPE_COMMAND=("hr" "-p" "info")
+# PENRUN_PIPE_COMMAND=("hr" "-p" "info")
 
 # Add Default arguments which be appended to each penrun run
 # Argument must be a bash array.
@@ -40,10 +45,10 @@ PENRUN_PIPE_COMMAND=("hr" "-p" "info")
 # Perform this function prior to each penrun run.
 # Can be used e.g. for power cycles or sending triggers
 pre_run() {
-    echo "I am a pre_run hook!"
+	echo "I am a pre_run hook!"
 }
 
 # Perform this function after to each penrun run.
 post_run() {
-    echo "I am a post_run hook!"
+	echo "I am a post_run hook!"
 }
